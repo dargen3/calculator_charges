b42181f2b98ea525da4ae469dc4af56f813e2cbe
dargen3 <dargen3@centrum.cz>
2018-04-26 13:45:22 +0200

    asdf

--- calculator_charges.py
+++ calculator_charges.py
@@ -60,43 +60,40 @@ if __name__ == "__main__":
         setm = Set_of_molecule(args.sdf_input)
         set_of_molecules = setm[:len(setm)]
         count = 0
-        electronegativity = {"C":2.55, "H":2.2, "O":3.44, "N":3.04, "S":2.58}
-        print("type,charge,highest_bond,el_BA,chg_BA,el_BBA,chg_BBA,total_chg,el_dist,el_dist_cor,total_chg_cor")
         for molecule in set_of_molecules:
-            for index, atom in enumerate(molecule._atoms):
+            for atom in molecule._atoms:
                 atom.charge = list_with_right_charges[count]
                 count += 1
-                atom.highest_bond = molecule._symbol_gravity[index][-1]
-                atom.num_of_BA = len(molecule._bonded_atoms[index+1])
-                atom.BA = molecule._bonded_atoms[index+1]
-                atom.num_of_BBA = len(molecule._bonded_bonded_atoms[index+1])
-                atom.BBA = molecule._bonded_bonded_atoms[index+1]
-                atom.el_BA = 0
-                atom.el_BBA = 0
-                atom.el_dist = 0
-                atom.el_dist_cor = 0
-                for aaatom in molecule._bonded_atoms[index+1]:
-                    atom.el_BA += electronegativity[molecule._atoms[aaatom-1].symbol]
-                    atom.el_dist += electronegativity[molecule._atoms[aaatom-1].symbol]/molecule.matrix_of_distance[index][aaatom-1]
-                    atom.el_dist_cor += (electronegativity[molecule._atoms[aaatom-1].symbol] - electronegativity[atom.symbol])/molecule.matrix_of_distance[index][aaatom-1]
-                for aaatom in molecule._bonded_bonded_atoms[index + 1]:
-                    atom.el_BBA += electronegativity[molecule._atoms[aaatom - 1].symbol]
-                    atom.el_dist += electronegativity[molecule._atoms[aaatom-1].symbol]/molecule.matrix_of_distance[index][aaatom-1]
-                    atom.el_dist_cor += (electronegativity[molecule._atoms[aaatom - 1].symbol] - electronegativity[atom.symbol]) / molecule.matrix_of_distance[index][aaatom - 1]
 
+
+        electronegativity = {"C":2.55, "H":2.2, "O":3.44, "N":3.04, "S":2.58}
         for molecule in set_of_molecules:
             for index, atom in enumerate(molecule._atoms):
-                atom.chg_BA = 0
-                atom.chg_BBA = 0
-                atom.total_chg = 0
-                atom.total_chg_cor = 0
-                for aaaa in atom.BA:
-                    atom.chg_BA += molecule._atoms[aaaa-1].charge
-                    atom.total_chg += molecule._atoms[aaaa-1].charge/molecule.matrix_of_distance[index][aaaa-1]
-                    atom.total_chg_cor += (molecule._atoms[aaaa-1].charge - atom.charge)/molecule.matrix_of_distance[index][aaaa-1]
-                for bbbb in atom.BBA:
-                    atom.chg_BBA += molecule._atoms[bbbb-1].charge
-                    atom.total_chg_cor += (molecule._atoms[aaaa-1].charge - atom.charge)/molecule.matrix_of_distance[index][aaaa-1]
-                    atom.total_chg += molecule._atoms[aaaa-1].charge/molecule.matrix_of_distance[index][bbbb-1]
-                a = atom
-                print("{},{},{},{},{},{},{},{},{},{},{}".format(a.symbol, a.charge, a.highest_bond, round(a.el_BA/a.num_of_BA,4), round(a.chg_BA/a.num_of_BA, 4), round(a.el_BBA/a.num_of_BBA,4), round(a.chg_BBA/a.num_of_BBA, 4), round(a.total_chg/(a.num_of_BBA+a.num_of_BA), 4), round(a.el_dist/(a.num_of_BBA+a.num_of_BA), 4), round((a.el_dist_cor/(a.num_of_BBA+a.num_of_BA)-electronegativity[a.symbol]), 4), round((a.total_chg_cor/(a.num_of_BBA+a.num_of_BA))-a.charge, 4)))
+                atom.highest_bond = molecule._symbol_gravity[index][-1]
+                atom.atoms_data = {2:[0,0,0],3:[0,0,0],4:[0,0,0],5:[0,0,0],6:[0,0,0],7:[0,0,0],8:[0,0,0],9:[0,0,0],10:[0,0,0],11:[0,0,0],12:[0,0,0]}
+                for i,a in enumerate(molecule._atoms):
+                    if i == index:
+                        continue
+                    for key in atom.atoms_data:
+                        if molecule.matrix_of_distance[i][index] < key:
+                            atom.atoms_data[key][0] += (electronegativity[a.symbol] - electronegativity[atom.symbol])/molecule.matrix_of_distance[index][i]
+                            atom.atoms_data[key][1] += 1
+                            atom.atoms_data[key][2] += (a.charge - atom.charge)/molecule.matrix_of_distance[index][i]
+        print("type,charge,highest_bond,el2,chg2,el3,chg3,el4,chg4,el5,chg5,el6,chg6,el7,chg7,el8,chg8,el9,chg9,el10,chg10,el11,chg11,el12,chg12")
+        for molecule in set_of_molecules:
+            for a in molecule._atoms:
+                print(a.symbol, end=",")
+                print(a.charge, end=",")
+                print(a.highest_bond, end=",")
+                for dist in range(2,13):
+                    print(a.atoms_data[dist][0]/a.atoms_data[dist][1], end=",")
+                    if dist != 12:
+                        print(a.atoms_data[dist][2]/a.atoms_data[dist][1], end=",")
+                    else:
+                        print(a.atoms_data[dist][2]/a.atoms_data[dist][1])
+
+
+
+
+
+
